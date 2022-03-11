9866b4e8d8fb1a1b76a6fa0b06457a0b15cc1bab
Anna <anna.dalgren@gmail.com>
2022-03-11 11:19:46 +0100

    centrerad

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { OpenWeatherMapService, GeoData } from '../open-weather-map.service';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { OpenWeatherMapService, OneCallData, WeatherData, WeatherDescription, GeoData } from '../open-weather-map.service';
 import { Output, EventEmitter } from '@angular/core';
 
 
@@ -8,11 +9,15 @@ import { Output, EventEmitter } from '@angular/core';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   @Output() sendGeoData: EventEmitter<GeoData> = new EventEmitter();
   submitted = false;
 
   model = {name: 'Ex.Lund'};
+
+  private url = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=1766139ca73a00ae488fad64f5a917eb';
+
+  posts : any;
 
   private city = "";
 
@@ -20,25 +25,33 @@ export class SearchComponent {
 
   locationList : GeoData[] = [];
 
-  constructor(private openWeather: OpenWeatherMapService) {}
+  links  = [this.url, this.url, this.url];
+  constructor(private http: HttpClient, private openWeather: OpenWeatherMapService) {
 
+  }
   showInfo(data : GeoData){
-    console.log("Emitting sendGeoData")
     console.log(data.lon + ' '+ data.lat);
     this.sendGeoData.emit(data);
     this.locationList = [];
   }
+  getPosts() {
+	return this.http.get(this.url);
+  }
+  ngOnInit(): void {
 
+  }
   showFormControls(form: any) {
     this.city = form && form.controls['name'] &&
     form.controls['name'].value;
   }
 
+
+
   onSubmit(){
+
     this.openWeather.requestGeoData(this.city).subscribe(
       (response: GeoData[]) => {
       	const res = response;
-        console.log(res);
         this.locationList = response;
       }
     );
